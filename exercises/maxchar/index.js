6868353541de7a057f68c02153caae5ceca700fd
Nick S <nicktschuck@gmail.com>
2020-09-06 19:14:37 -0700

    maxchar initial development

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    var charCounts = {};
+        maxKey = '';
+
+    for(let char of str) {
+        if(!charCounts[char]) {
+            charCounts[char] = 0;
+        }
+        charCounts[char]++;
+        if(maxKey == '' || charCounts[char] > charCounts[maxKey]){
+            maxKey = char;
+        }
+    }
+
+    return maxKey;
+}
 
 module.exports = maxChar;
