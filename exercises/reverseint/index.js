71d317a1d1a76af798a764871708345970dacba9
Nick S <nicktschuck@gmail.com>
2020-09-06 18:31:33 -0700

    Refactored reverseint to not convert to string/arrays/etc. Left first solution commented out for reference

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,13 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {
-    return parseInt(n
-        .toString()
-        .split("")
-        .reverse()
-        .join("")) 
-        * Math.sign(n);
+function reverseInt(n) {   
+    const original_n = n;
+    let rev = 0, 
+        digit;
+
+    n = Math.abs(n)
+    while(n > 0) {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n = Math.trunc(n / 10);
+    }
+    return rev * Math.sign(original_n);
 }
+
+
+//First Solution
+// function reverseInt(n) {
+//     return parseInt(n
+//         .toString()
+//         .split("")
+//         .reverse()
+//         .join("")) 
+//         * Math.sign(n);
+// }
 
 module.exports = reverseInt;
