e88ce67924f5e6af4905d90727cc5dd864d9dd3f
Nick S <nicktschuck@gmail.com>
2020-09-07 10:55:04 -0700

    anagrams intial development

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(string) {
+    return string
+        .replace("/[^\w]/g", "")
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join("");
+}
 
 module.exports = anagrams;
+
+//First Solution
+// function anagrams(stringA, stringB) {
+//     const hashMapA = hashStringCharacters(stringA);
+//     const hashMapB = hashStringCharacters(stringB);
+
+//     //Character maps have different amount of keys
+//     if (Object.keys(hashMapA).length !== Object.keys(hashMapA).length) return false;
+
+//     for (let char in hashMapA) {
+//         if (hashMapA[char] !== hashMapB[char]) return false;
+//     }
+//     return true;
+// }
+
+// function hashStringCharacters(string) {
+//     let hashMap = {};
+//     for (let char of string.replace("/[^\w]/g", "").toLowerCase()) {
+//             hashMap[char] = hashMap[char] + 1 || 1;
+//     }
+//     return hashMap;
+// }
