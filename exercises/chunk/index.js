2bec45ad8e1dc9fd008dad29b7575dd462af5e7b
Nick S <nicktschuck@gmail.com>
2020-09-07 10:08:52 -0700

    chunk algorithm refactor

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,19 +10,29 @@
 
 function chunk(array, size) {
     const arrayOfChunks = [];
-    for(let element of array) {
-        const last = arrayOfChunks[arrayOfChunks.length - 1];
-        
-        if (!last || last.length === size) {
-            arrayOfChunks.push([element]);
-        }
-        else {
-            last.push(element);
-        }
+    let index = 0;
+    while(index < array.length) {
+        arrayOfChunks.push(array.slice(index, index + size));
+        index += size;
     }
     return arrayOfChunks;
 }
 
-chunk([1,2, 3, 4, 5, 6, 7, 8], 2);
+
+// function chunk(array, size) {
+//     const arrayOfChunks = [];
+//     for(let element of array) {
+//         const last = arrayOfChunks[arrayOfChunks.length - 1];
+        
+//         if (!last || last.length === size) {
+//             arrayOfChunks.push([element]);
+//         }
+//         else {
+//             last.push(element);
+//         }
+//     }
+//     return arrayOfChunks;
+// }
+
 
 module.exports = chunk;
